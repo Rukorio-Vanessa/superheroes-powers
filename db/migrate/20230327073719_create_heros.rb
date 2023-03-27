0ba0304e111212dfe3a082114d807f9662eae2fa
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-03-27 10:47:54 +0300

    generated Power model and migration

--- db/migrate/20230327073719_create_heros.rb
+++ db/migrate/20230327073719_create_heros.rb
@@ -1,8 +1,8 @@
-class CreateHeros < ActiveRecord::Migration[7.0]
+class CreateHeroes < ActiveRecord::Migration[7.0]
   def change
-    create_table :heros do |t|
-      t.string :name
-      t.string :super_name
+    create_table :heroes do |t|
+      t.string :name, null: false
+      t.string :super_name, null:false
 
       t.timestamps
     end
