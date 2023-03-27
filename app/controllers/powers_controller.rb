56d4c2d3e2c09160023910f3dff87be23583c7e4
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-03-27 18:36:18 +0300

    added #show mthd to power controller

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,21 @@
 class PowersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
     def index
         render json: Power.all, status: :ok
+    end
+
+    def show
+        power = find_power
+        render json: power, status: :ok
+    end
+
+    private
+    def find_power
+        Power.find(params[:id])
+    end
+
+    def render_not_found_response
+        render json: { error: "Power not found" }, status: :not_found
     end
 end
