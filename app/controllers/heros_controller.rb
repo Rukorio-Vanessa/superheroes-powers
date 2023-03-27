109b69bd676fc5e76b26423328e814cefb80588a
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-03-27 18:23:50 +0300

    added #show method to herocontroller

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,5 +1,21 @@
 class HerosController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
     def index 
         render json: Hero.all, status: :ok
+    end
+
+    def show
+        hero = find_hero
+        render json: hero, status: :ok
+    end
+
+    private
+    def find_hero
+        Hero.find(params[:id])
+    end
+
+    def render_not_found_response
+        render json: { error: "Hero not found" }, status: :not_found
     end
 end
--- app/serializers/hero_serializer.rb
+++ app/serializers/hero_serializer.rb
@@ -1,6 +1,5 @@
 class HeroSerializer < ActiveModel::Serializer
   attributes :id, :name, :super_name
 
-  has_many :hero_powers
   has_many :powers, through: :hero_powers
 end
