4b523fdc34fa6070eb427af22ebac26ee8f5588c
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-03-27 14:25:59 +0300

    generated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Seeding superheroes 🦸..."
+
+18.times do
+    Hero.create(
+        name: Faker::Name.name,
+        super_name: Faker::Superhero.name,
+    )
+end
+
+18.times do
+    Power.create(
+        name: Faker::Superhero.power,
+        description: Faker::Lorem.paragraph_by_chars(number: 21)
+    )
+end
+
+
+puts "Done!To Infinity and Beyond! ⚡"
+
