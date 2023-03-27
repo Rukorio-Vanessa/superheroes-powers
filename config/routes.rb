055cbbc86a494d8bfeb0b9299db5581187eae37b
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-03-27 14:43:29 +0300

    defined routes for heroes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  #Heroes
+  get '/heros', to: 'heros#index'
+  get '/heros/:id', to: 'heros#show'
+
 end
